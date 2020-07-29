f93e0146cb63d1d11dbdadf8c1edc324b43abdb0
aneta-s <54153719+aneta-s@users.noreply.github.com>
2020-07-29 09:37:43 +0200

    Updates

---
+++ precache-manifest.64baae488ad1b2a5d79866fe643887c1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c094327858f304835aaecd7f3851c4b4",
+    "url": "/recognition-app/index.html"
+  },
+  {
+    "revision": "c270498d4541f10f9bb8",
+    "url": "/recognition-app/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "5285a90ed8e2f9eef4e9",
+    "url": "/recognition-app/static/css/main.7cba6051.chunk.css"
+  },
+  {
+    "revision": "c270498d4541f10f9bb8",
+    "url": "/recognition-app/static/js/2.7a6106ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recognition-app/static/js/2.7a6106ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5285a90ed8e2f9eef4e9",
+    "url": "/recognition-app/static/js/main.9a55f178.chunk.js"
+  },
+  {
+    "revision": "e665104611ea3cf1df45",
+    "url": "/recognition-app/static/js/runtime-main.ec335661.js"
+  },
+  {
+    "revision": "76f28f4fb56c84ba619a78700cec5965",
+    "url": "/recognition-app/static/media/brain.76f28f4f.png"
+  }
+]);
